e81d1047147827b79e35dd702f7a11954ffc2fcd
ospych <chyngyz.osmonov@financesoft.kg>
2024-07-30 19:19:56 +0600

    Fix: ArrayHelper bug fix

--- src/ArrayHelper.java
+++ src/ArrayHelper.java
@@ -1,10 +1,14 @@
+import java.util.*;
+
 public class ArrayHelper {
     private int[] array;
     private int lastInitializedIndex;
+    private int size;
 
     public ArrayHelper(int size) {
         array = new int[size];
         lastInitializedIndex = -1;
+        this.size = size;
     }
 
 
@@ -41,6 +45,47 @@ public class ArrayHelper {
 
     public int[] getArray() {
         return array;
+    }
+
+    public int[] sortByMax() {
+        return Arrays.stream(array).sorted().toArray();
+    }
+
+    public int[] sortByMin() {
+        Arrays.sort(Arrays.stream(array).boxed().toArray(), Collections.reverseOrder());
+        return array;
+    }
+
+    public int getMinElement() {
+        return Arrays.stream(array).min().getAsInt();
+    }
+
+    public int getMaxElement() {
+        return Arrays.stream(array).max().getAsInt();
+    }
+
+    public int getIndexOfMinElement() {
+        int element = Arrays.stream(array).min().getAsInt();
+        return getIndexByElement(element);
+    }
+
+    public int geIndexOftMaxElement() {
+        int element = Arrays.stream(array).max().getAsInt();
+        return getIndexByElement(element);
+    }
+
+    public int getIndexByElement(int element) {
+        return Arrays.stream(array).boxed().toList().indexOf(element);
+    }
+
+    public void generateWithRandom() {
+        int[] tempArray = new int[size];
+        Random rnd = new Random();
+
+        for (int i = 0; i < tempArray.length; i++) {
+            tempArray[i] = rnd.nextInt(0, 260);
+        }
+        array = tempArray;
     }
 
     private int[] createNewIfLimit(int size) {
--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,48 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arrayInt = new int[300];
-        Random rnd = new Random();
+//        int[] arrayInt = new int[300];
+//        Random rnd = new Random();
+//
+//        for (int i = 0; i < arrayInt.length; i++) {
+//            arrayInt[i] = rnd.nextInt(0, 260);
+//        }
+//
+//        similarElement(arrayInt);
 
-        for (int i = 0; i < arrayInt.length; i++) {
-            arrayInt[i] = rnd.nextInt(0, 260);
+        ArrayHelper helper = new ArrayHelper(10);
+        helper.generateWithRandom();
+        System.out.println("Array: " + Arrays.toString(helper.getArray()));
+        System.out.println("Sort by max: " + Arrays.toString(helper.sortByMax()));
+        System.out.println("Sort by min: " + Arrays.toString(helper.sortByMin()));
+        System.out.println("Index of max element: " + helper.geIndexOftMaxElement());
+        System.out.println("Index of min element: " + helper.getIndexOfMinElement());
+        System.out.println("Max element: " + helper.getMaxElement());
+        System.out.println("Min element: " + helper.getMinElement());
+
+        int min = helper.getArray()[0];
+        int max = helper.getArray()[0];
+        int minIndex = 0;
+        int maxIndex = 0;
+        for (int i = 0; i < helper.getArray().length; i++) {
+            if (min > helper.getArray()[i]) {
+                min = helper.getArray()[i];
+                minIndex = i;
+            }
+
+            if (max < helper.getArray()[i]) {
+                max = helper.getArray()[i];
+                maxIndex = i;
+            }
         }
 
-        similarElement(arrayInt);
+        System.out.println(min);
+        System.out.println(max);
+        System.out.println(minIndex);
+        System.out.println(maxIndex);
+
     }
+
 
     private static void similarElement(int[] arrayInt) {
         List<Integer> similar = new ArrayList<>();
