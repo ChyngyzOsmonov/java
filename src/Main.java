b323d67a121c9de598507c3a44f60a161ea378c7
ospych <chyngyz.osmonov@financesoft.kg>
2024-08-15 19:40:07 +0600

    Feat: Lesson 11

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,35 @@
+import java.util.Arrays;
+import java.util.Random;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        // primitive values: byte, short, int, long, double, float, boolean, char
+        Random rnd = new Random();
+        int[] array = new int[20];
+
+        for (int i = 0; i <array.length; i++) {
+            array[i] = rnd.nextInt(1, 101);
+        }
+
+        System.out.println(Arrays.toString(getNumberByThreeDivided(array)));
+    }
+
+    public static int[] getNumberByThreeDivided(int[] array) {
+        int count = 0;
+        for (int j : array) {
+            if (j % 3 == 0) {
+                count++;
+            }
+        }
+        int[] tempArray = new int[count];
+        int a = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 3 == 0) {
+                tempArray[a] = array[i];
+                a++;
+            }
+        }
+        return tempArray;
     }
 }
